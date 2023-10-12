048c20835b1454e65fe7f05beefcbad85554fc6a
Shiaba <>
2023-10-12 15:38:05 +0000

    Initial commit

---
+++ js/myscript.js
@@ -0,0 +1,12 @@
+$(document).ready(function () {
+    $("h2").css("text-decoration", "underline"); //underlines all <h2> elements
+    $("ul").css("border", "solid 1px #ccc"); //adds a border to all lists
+
+    //Add class
+    //$("h2").addClass("underline"); //underlines all <h2> elements
+    //$("ul").addClass("border"); //adds a border to all lists
+
+    //Remove class
+    //$("h2").removeClass("underline"); //underlines all <h2> elements
+    //$("ul").removeClass("border"); //adds a border to all lists
+});
